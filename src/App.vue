045755acc1a1a430ba2a306ac626c6f81e777790
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-30 16:32:47 +0800

    feat: KLineHeader ElSelect

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <KLineHeader
+      :symbol="symbol"
+      :symbolList="symbolList.value"
+      @symbolHanlder="symbolHanlder"
+    />
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import KLineHeader from '@/components/KLineHeader'
+import { ref, onMounted, reactive } from 'vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    KLineHeader,
+    // HelloWorld
+  },
+  setup() {
+    const symbolList = reactive({})
+    const symbol = ref('')
+    const symbolInfo = ref({})
+    const kLineRef = ref(null)
+    onMounted(async () => {
+
+    const options = [
+      {
+        value: 'Option1',
+        pair: 'Option1',
+      },
+      {
+        value: 'Option2',
+        pair: 'Option2',
+      },
+      {
+        value: 'Option3',
+        pair: 'Option3',
+      },
+      {
+        value: 'Option4',
+        pair: 'Option4',
+      },
+      {
+        value: 'Option5',
+        pair: 'Option5',
+      },
+    ]
+      symbolList.value = options
+      symbol.value = 'symbol'
+      symbolInfo.value = options[0]
+    })
+    const symbolHanlder = (e) => {
+      symbol.value = e
+      kLineRef.value.setSymbol(e)
+    }
+
+    return {
+      symbol,
+      symbolList,
+      symbolInfo,
+      symbolHanlder,
+      kLineRef,
+    }
+  },
 }
 </script>
 
---
+++ src/components/KLineHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-header>
+    <el-select
+      v-model="symbol"
+      filterable
+      :placeholder="'plzChoicePair'"
+      @change="changeHallder"
+    >
+      <el-option
+        v-for="item in symbolList"
+        :key="item['quote-currency']"
+        :label="item.pair"
+        :value="item['base-currency'] + item['quote-currency']"
+      />
+    </el-select>
+  </el-header>
+</template>
+<script>
+  import { ElSelect, ElOption } from 'element-plus'
+  export default {
+    name: 'KLineHeader',
+    components: { ElSelect, ElOption },
+    props: {
+      symbol: {
+        type: String,
+        required: true,
+      },
+      symbolList: {
+        type: Array,
+        required: true,
+      },
+    },
+    emits: ['symbolHanlder'],
+    setup(props, { emit }) {
+      const changeHallder = (e) => emit('symbolHanlder', e)
+      return {
+        changeHallder,
+      }
+    },
+  }
+</script>
+<style scoped>
+  .el-header {
+    background: #131625;
+    display: flex;
+    padding: 15px;
+    align-items: center;
+    border-bottom: 1px solid black;
+  }
+  ::v-deep(.el-input__inner) {
+    background-color: #21253c;
+    color: #f8f8f8;
+    border-color: #000;
+  }
+</style>
